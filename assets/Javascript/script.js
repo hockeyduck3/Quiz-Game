f0f4c49d437126ca426e94927ea5785cd9896c7c
LJ Spencer <hockeyduck3@gmail.com>
2020-03-22 15:17:47 -0600

    Changed up the wording on the Google pixel question. Also added a way to block the player from clicking the correct button more than once to rack up points. Finally, added some code to make the answer buttons be in random positions.

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,6 +1,7 @@
 var startP = document.querySelector('.startParagraph');
 var startBtn = document.querySelector('.start-btn');
 var firstBtn = document.querySelector('.first-btn');
+var gameBtn = document.querySelector('.game-btn');
 var nextBtn = document.querySelector('.next-btn');
 var instructions = document.querySelector('.instructions');
 var highscoreLink = document.querySelector('.hsLink');
@@ -82,12 +83,16 @@ function questionsFunc() {
     cardTitle.textContent = `Question ${index+1}`;
     questionTitle.textContent = questions[index].question;
 
-    
-    for (var i = 0; i < 4; i++) {
-        document.querySelector(`.game-btn-${i}`).textContent = questions[index].answers[i].item;
+    // With this array and the sort function beneath it, it will allow the buttons to be random instead of being in static postions every game.
+    var randomNumber = [0,1,2,3];
+    randomNumber.sort(() => Math.random() - 0.5);
+
+    for (var i = 0; i < randomNumber.length; i++) {
+        document.querySelector(`.game-btn-${randomNumber[i]}`).textContent = questions[index].answers[i].item;
+        document.querySelector(`.game-btn-${i}`).classList.add('enabled');
 
         if (questions[index].answers[i].true) {
-            document.querySelector(`.game-btn-${i}`).dataset.correct = 'true'
+            document.querySelector(`.game-btn-${randomNumber[i]}`).dataset.correct = 'true'
           }
     }
 }
@@ -116,24 +121,36 @@ function nextQuestion() {
 // Check Answers Function
 function checkAnswerFunc(event) {
     if (event.target.matches('button')) {
-        for (var i = 0; i < 4; i++) {
-            if (document.querySelector(`.game-btn-${i}`).dataset.correct === 'true') {
-                document.querySelector(`.game-btn-${i}`).classList.add('correct');
+        // This if statement makes it so that the player can't click on the correct answer and countinue to rack up points after the point have already been given.
+        if (gameBtn.classList.contains('enabled')) {
+            for (var i = 0; i < 4; i++) {
+                // This line of code will remove the class of enabled from all the answer so the user can only get points once per question.
+                document.querySelector(`.game-btn-${i}`).classList.remove('enabled');
+
+                // This will check and see which answer was the correct one and add the class 'correct' on it so that will it'll show up green for the user. While every other answer will show up as red.
+                if (document.querySelector(`.game-btn-${i}`).dataset.correct === 'true') {
+                    document.querySelector(`.game-btn-${i}`).classList.add('correct');
+                } else {
+                    document.querySelector(`.game-btn-${i}`).classList.add('wrong');
+                }
+            }
+    
+            nextBtn.classList.remove('hide');
+    
+            if (event.target.dataset.correct === 'true') {
+                cardTitle.textContent = 'Correct!'
+                score += 5;
             } else {
-                document.querySelector(`.game-btn-${i}`).classList.add('wrong');
+                cardTitle.textContent = 'Wrong...'
+                score -= 5;
             }
         }
-
-        nextBtn.classList.remove('hide');
-
-        if (event.target.dataset.correct === 'true') {
-            cardTitle.textContent = 'Correct!'
-            score += 5;
-        } else {
-            cardTitle.textContent = 'Wrong...'
-            score -= 5;
-        }
     }
+}
+
+// Stop the game function
+function stopGame() {
+    
 }
 
 // Questions/Answers
@@ -157,12 +174,15 @@ var questions = [
         ]
     },
     {
-        question: 'What year did Google release the first Pixel phone?',
+        question: 'What day did Google release the first Pixel phone?',
         answers: [
-            {item: '2006', true: false},
-            {item: '2011', true: false},
-            {item: '1999', true: false},
-            {item: '2016', true: true}
+            {item: 'August 16th, 2006', true: false},
+            {item: 'July 4th, 2011', true: false},
+            {item: 'November 2nd, 1999', true: false},
+            {item: 'October 20th, 2016', true: true}
         ]
-    }
+    },
+    // {
+    //     question: '',
+    // }
 ]
